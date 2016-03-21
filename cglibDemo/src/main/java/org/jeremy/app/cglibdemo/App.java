86a600de35dad00dea9788975a8691ec682b593c
CutePrince30 <jaysunyun_361@163.com>
2016-03-21 13:40:07 +0800

    format

--- cglibDemo/src/main/java/org/jeremy/app/cglibdemo/App.java
+++ cglibDemo/src/main/java/org/jeremy/app/cglibdemo/App.java
@@ -4,10 +4,11 @@ package org.jeremy.app.cglibdemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");
+	}
+	
 }
